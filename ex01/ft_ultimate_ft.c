282038a49c0675dd5ba7beeecc395338e1729a95
Shelton Adams <shelton@Sheltons-MacBook-Air.local>
2020-06-19 23:15:49 +0200

    exercise 01

--- ex01/ft_ultimate_ft.c
+++ ex01/ft_ultimate_ft.c
@@ -11,28 +11,8 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
 
 void ft_ultimate_ft(int *********nbr)
 {
 	*********nbr = 42;
 }
-
-int main(void)
-{
-	int a = 15;
-	int *b = &a;
-	int **c = &b;
-	int ***d = &c;
-	int ****e = &d;
-	int *****f = &e;
-	int ******g = &f;
-	int *******h = &g;
-	int ********i = &h;
-	int *********j = &i;
-
-	ft_ultimate_ft(j);
-	printf("%d\n", a);
-	return (0);
-}
-
