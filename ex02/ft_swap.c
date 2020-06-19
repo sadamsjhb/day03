e0c66a9c280d25a0ce88c6dbffbcd9e3b9ae79ae
Shelton Adams <shelton@Sheltons-MacBook-Air.local>
2020-06-19 23:53:56 +0200

    exercise 02

--- ex02/ft_swap.c
+++ ex02/ft_swap.c
@@ -1,5 +1,4 @@
 #include <unistd.h>
-#include <stdio.h>
 
 void ft_swap(int *a, int *b)
 {
@@ -8,13 +7,4 @@ int c;
 c = *a;
 *a = *b;
 *b = c;
-}
-
-int main()
-{
-int a = 4;
-int b = 9;
-ft_swap(&a, &b);
-printf("%d, %d\n", a, b);
-return (0);
 }
