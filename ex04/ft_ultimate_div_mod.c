31787cff968a24f5d085c8a1cff541825a99f238
shelton Adams <sadams@c4r9s3.wethinkcode.co.za>
2020-06-21 13:03:59 +0200

    exrcise 04

--- ex04/ft_ultimate_div_mod.c
+++ ex04/ft_ultimate_div_mod.c
@@ -6,12 +6,11 @@
 /*   By: sadams <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/21 11:25:07 by sadams            #+#    #+#             */
-/*   Updated: 2020/06/21 12:58:35 by sadams           ###   ########.fr       */
+/*   Updated: 2020/06/21 13:03:30 by sadams           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
 
 void ft_ultimate_div_mod(int *a, int *b)
 {
@@ -26,14 +25,4 @@ void ft_ultimate_div_mod(int *a, int *b)
 		*a = div / mod;
 		*b = div % mod;
 	}
-}
-
-int main(void)
-{
-	int a = 15;
-	int b = 4;
-
-	ft_ultimate_div_mod(&a, &b);
-	printf("%d, %d\n", a, b);
-	return (0);
 }
