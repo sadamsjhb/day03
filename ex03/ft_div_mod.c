5365ffa8908690bdf99c80378d79a5d5bf0e0ca9
shelton Adams <sadams@c4r9s3.wethinkcode.co.za>
2020-06-21 10:01:58 +0200

    exercise 03

--- ex03/ft_div_mod.c
+++ ex03/ft_div_mod.c
@@ -1,20 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadams <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/20 09:24:54 by sadams            #+#    #+#             */
+/*   Updated: 2020/06/21 09:59:37 by sadams           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <stdio.h>
 
 void ft_div_mod(int a, int b, int *div, int *mod)
 {
-if(b != 0)
-{
-*div = a / b;
-*mod = a % b;
-}
+	if(b != 0)
+	{
+		*div = a / b;
+		*mod = a % b;
+	}
 }
 
 int main(void)
 {
-int a = 7;
-int b = 2;
-ft_div_mod(&a,&b);
-printf("%d, %d\n", *div, *mod);
-return (0);
+	int a = 7;
+	int b = 2;
+	int div;
+	int mod;
+
+	ft_div_mod(a, b, &div, &mod);
+	printf("%d, %d\n", div, mod);
+	return (0);
 }
